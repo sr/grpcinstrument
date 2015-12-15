64768221d4ab053e5f892ad428409b3d113ebabc
Simon Rozet <me@simonrozet.com>
2015-12-15 00:43:33 +0000

    change Instrumentator interface wrt to metrics

--- grpcinstrument.go
+++ grpcinstrument.go
@@ -1,7 +1,6 @@
 package grpcinstrument
 
 import (
-	"fmt"
 	"time"
 
 	"go.pedge.io/proto/time"
@@ -9,11 +8,7 @@ import (
 
 type Instrumentator interface {
 	Log(*Call)
-	Increment(counterName string)
-}
-
-func (c *Call) IsError() bool {
-	return c.Error != nil
+	CollectMetrics(*Call)
 }
 
 func Instrument(
@@ -35,11 +30,10 @@ func Instrument(
 	if err != nil {
 		call.Error = &Error{Message: err.Error()}
 	}
-	metric := fmt.Sprintf("%s.methods.%s", call.Service, call.Method)
-	if call.IsError() {
-		instrumentator.Increment(fmt.Sprintf("%s.errors", metric))
-	}
-	instrumentator.Increment(fmt.Sprintf("%s.calls", metric))
-	instrumentator.Increment(fmt.Sprintf("%s.success", metric))
+	instrumentator.CollectMetrics(call)
 	instrumentator.Log(call)
+}
+
+func (c *Call) IsError() bool {
+	return c.Error != nil
 }
